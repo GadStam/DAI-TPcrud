899dbe724cfed0df5536649b32439436b0504726
EITAN DANIEL <46028383@ort>
2022-04-27 09:16:08 -0300

    27/04/22

--- src/services/personajeService.js
+++ src/services/personajeService.js
@@ -21,34 +21,48 @@ export class PersonajeService {
 
         const pool = await sql.connect(config);
         const response = await pool.request()
-            .input('Nombre',sql.VarChar, personaje?.nombre ?? '')
-            .input('Imagen',sql.VarChar, personaje?.imagen ?? '')
-            .input('Edad',sql.Int, personaje?.edad ?? 0)
-            .input('Peso',sql.Float, personaje?.peso ?? 0)
-            .input('Historia',sql.VarChar, personaje?.historia ?? '')
-            .input('Nacimiento',sql.DateTime, personaje?.nacimiento ?? '')
+            .input('Nombre',sql.VarChar, personaje?.Nombre ?? '')
+            .input('Imagen',sql.VarChar, personaje?.Imagen ?? '')
+            .input('Edad',sql.Int, personaje?.Edad ?? 0)
+            .input('Peso',sql.Float, personaje?.Peso ?? 0)
+            .input('Historia',sql.VarChar, personaje?.Historia ?? '')
+            .input('Nacimiento',sql.VarChar, personaje?.Nacimiento ?? '')
             .query(`INSERT INTO ${personajeTabla}(Nombre, Imagen, Edad, Peso, Historia, Nacimiento) VALUES (@Nombre, @Imagen, @Edad, @Peso, @Historia, @Nacimiento)`);
         console.log(response)
 
         return response.recordset;
     }
 
-    updatePizzaById = async (id, pizza) => {
+    updatePersonajeById = async (id, personaje) => {
         console.log('This is a function on the service');
 
         const pool = await sql.connect(config);
         const response = await pool.request()
             .input('id',sql.Int, id)
-            .input('Nombre',sql.VarChar, personaje?.nombre ?? '')
-            .input('Imagen',sql.VarChar, personaje?.imagen ?? '')
-            .input('Edad',sql.Int, personaje?.edad ?? 0)
-            .input('Peso',sql.Float, personaje?.peso ?? 0)
-            .input('Historia',sql.VarChar, personaje?.historia ?? '')
-            .input('Nacimiento',sql.VarChar, personaje?.nacimiento ?? '')
+            .input('Nombre',sql.VarChar, personaje?.Nombre ?? '')
+            .input('Imagen',sql.VarChar, personaje?.Imagen ?? '')
+            .input('Edad',sql.Int, personaje?.Edad ?? 0)
+            .input('Peso',sql.Float, personaje?.Peso ?? 0)
+            .input('Historia',sql.VarChar, personaje?.Historia ?? '')
+            .input('Nacimiento',sql.VarChar, personaje?.Nacimiento ?? '')
             .query(`UPDATE Personajes SET Nombre = @Nombre, Imagen = @Imagen, Edad = @Edad, Peso = @Peso, Historia = @Historia, Nacimiento = @Nacimiento WHERE Id = @Id`);
         console.log(response)
 
         return response.recordset;
     }
+
+    deletePersonajeById = async (id) => {
+        console.log('This is a function on the service');
+
+        const pool = await sql.connect(config);
+        const response = await pool.request()
+            .input('Id',sql.Int, id)
+            .query(`DELETE FROM ${personajeTabla} WHERE Id = @Id`);
+        console.log(response)
+
+        return response.recordset;
+    }
+
+
 
 }
